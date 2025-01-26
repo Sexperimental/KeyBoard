56821b231659a10c7065ac593f34d8da487a4035
experimental <65273445+sexperimental@users.noreply.github.com>
2025-01-26 21:18:50 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,286 @@
+document.getElementById('key1').addEventListener('click', function() {
+    var textbox = document.getElementById('textbox');
+    textbox.value += '~';
+});
+
+document.getElementById('key2').addEventListener('click', function() {
+    var textbox = document.getElementById('textbox');
+    textbox.value += '1';
+});
+
+document.getElementById('key3').addEventListener('click', function() {
+    var textbox = document.getElementById('textbox');
+    textbox.value += '2';
+});
+
+document.getElementById('key4').addEventListener('click', function() {
+    var textbox = document.getElementById('textbox');
+    textbox.value += '3';
+});
+
+document.getElementById('key5').addEventListener('click', function() {
+    var textbox = document.getElementById('textbox');
+    textbox.value += '4';
+});
+
+document.getElementById('key6').addEventListener('click', function() {
+    var textbox = document.getElementById('textbox');
+    textbox.value += '5';
+});
+
+document.getElementById('key7').addEventListener('click', function() {
+    var textbox = document.getElementById('textbox');
+    textbox.value += '6';
+});
+
+document.getElementById('key8').addEventListener('click', function() {
+    var textbox = document.getElementById('textbox');
+    textbox.value += '7';
+});
+
+document.getElementById('key9').addEventListener('click', function() {
+    var textbox = document.getElementById('textbox');
+    textbox.value += '8';
+});
+
+document.getElementById('key10').addEventListener('click', function() {
+    var textbox = document.getElementById('textbox');
+    textbox.value += '9';
+});
+
+document.getElementById('key11').addEventListener('click', function() {
+    var textbox = document.getElementById('textbox');
+    textbox.value += '0';
+});
+
+document.getElementById('key12').addEventListener('click', function() {
+    var textbox = document.getElementById('textbox');
+    textbox.value += '-';
+});
+
+//back space
+document.getElementById('key13').addEventListener('click', function() {
+    var textbox = document.getElementById('textbox');
+    textbox.value = textbox.value.slice(0, -1);
+});
+
+//tab
+document.getElementById('key14').addEventListener('click', function() {
+    var textbox = document.getElementById('textbox');
+    textbox.value += '';
+});
+
+document.getElementById('key15').addEventListener('click', function() {
+    var textbox = document.getElementById('textbox');
+    textbox.value += 'q';
+});
+
+document.getElementById('key16').addEventListener('click', function() {
+    var textbox = document.getElementById('textbox');
+    textbox.value += 'w';
+});
+
+document.getElementById('key17').addEventListener('click', function() {
+    var textbox = document.getElementById('textbox');
+    textbox.value += 'e';
+});
+
+document.getElementById('key18').addEventListener('click', function() {
+    var textbox = document.getElementById('textbox');
+    textbox.value += 'r';
+});
+
+document.getElementById('key19').addEventListener('click', function() {
+    var textbox = document.getElementById('textbox');
+    textbox.value += 't';
+});
+
+document.getElementById('key20').addEventListener('click', function() {
+    var textbox = document.getElementById('textbox');
+    textbox.value += 'y';
+});
+
+document.getElementById('key21').addEventListener('click', function() {
+    var textbox = document.getElementById('textbox');
+    textbox.value += 'u';
+});
+
+document.getElementById('key22').addEventListener('click', function() {
+    var textbox = document.getElementById('textbox');
+    textbox.value += 'i';
+});
+
+document.getElementById('key23').addEventListener('click', function() {
+    var textbox = document.getElementById('textbox');
+    textbox.value += 'o';
+});
+
+document.getElementById('key24').addEventListener('click', function() {
+    var textbox = document.getElementById('textbox');
+    textbox.value += 'p';
+});
+
+document.getElementById('key25').addEventListener('click', function() {
+    var textbox = document.getElementById('textbox');
+    textbox.value += '@';
+});
+
+document.getElementById('key26').addEventListener('click', function() {
+    var textbox = document.getElementById('textbox');
+    textbox.value += "\\"; 
+});
+
+//caps
+document.getElementById('key27').addEventListener('click', function() {
+    var textbox = document.getElementById('textbox');
+    textbox.value += '';
+});
+
+document.getElementById('key28').addEventListener('click', function() {
+    var textbox = document.getElementById('textbox');
+    textbox.value += 'a';
+});
+
+document.getElementById('key29').addEventListener('click', function() {
+    var textbox = document.getElementById('textbox');
+    textbox.value += 's';
+});
+
+document.getElementById('key30').addEventListener('click', function() {
+    var textbox = document.getElementById('textbox');
+    textbox.value += 'd';
+});
+
+document.getElementById('key31').addEventListener('click', function() {
+    var textbox = document.getElementById('textbox');
+    textbox.value += 'f';
+});
+
+document.getElementById('key32').addEventListener('click', function() {
+    var textbox = document.getElementById('textbox');
+    textbox.value += 'g';
+});
+
+document.getElementById('key33').addEventListener('click', function() {
+    var textbox = document.getElementById('textbox');
+    textbox.value += 'h';
+});
+
+document.getElementById('key34').addEventListener('click', function() {
+    var textbox = document.getElementById('textbox');
+    textbox.value += 'j';
+});
+
+document.getElementById('key35').addEventListener('click', function() {
+    var textbox = document.getElementById('textbox');
+    textbox.value += 'k';
+});
+
+document.getElementById('key36').addEventListener('click', function() {
+    var textbox = document.getElementById('textbox');
+    textbox.value += 'l';
+});
+
+document.getElementById('key37').addEventListener('click', function() {
+    var textbox = document.getElementById('textbox');
+    textbox.value += ':';
+});
+
+//enter
+document.getElementById('key38').addEventListener('click', function() {
+    var textbox = document.getElementById('textbox');
+    textbox.value += '';
+});
+
+//shift
+document.getElementById('key39').addEventListener('click', function() {
+    var textbox = document.getElementById('textbox');
+    textbox.value += '';
+});
+
+document.getElementById('key40').addEventListener('click', function() {
+    var textbox = document.getElementById('textbox');
+    textbox.value += 'z';
+});
+
+document.getElementById('key41').addEventListener('click', function() {
+    var textbox = document.getElementById('textbox');
+    textbox.value += 'x';
+});
+
+document.getElementById('key42').addEventListener('click', function() {
+    var textbox = document.getElementById('textbox');
+    textbox.value += 'c';
+});
+
+document.getElementById('key43').addEventListener('click', function() {
+    var textbox = document.getElementById('textbox');
+    textbox.value += 'v';
+});
+
+document.getElementById('key44').addEventListener('click', function() {
+    var textbox = document.getElementById('textbox');
+    textbox.value += 'b';
+});
+
+document.getElementById('key45').addEventListener('click', function() {
+    var textbox = document.getElementById('textbox');
+    textbox.value += 'n';
+});
+
+document.getElementById('key46').addEventListener('click', function() {
+    var textbox = document.getElementById('textbox');
+    textbox.value += 'm';
+});
+
+document.getElementById('key47').addEventListener('click', function() {
+    var textbox = document.getElementById('textbox');
+    textbox.value += '.';
+});
+
+document.getElementById('key48').addEventListener('click', function() {
+    var textbox = document.getElementById('textbox');
+    textbox.value += '/';
+});
+
+//right shit
+document.getElementById('key49').addEventListener('click', function() {
+    var textbox = document.getElementById('textbox');
+    textbox.value += '';
+});
+
+//ctrl
+document.getElementById('key50').addEventListener('click', function() {
+    var textbox = document.getElementById('textbox');
+    textbox.value += '';
+});
+
+//win
+document.getElementById('key51').addEventListener('click', function() {
+    var textbox = document.getElementById('textbox');
+    textbox.value += '';
+});
+
+//alt
+document.getElementById('key52').addEventListener('click', function() {
+    var textbox = document.getElementById('textbox');
+    textbox.value += '';
+});
+
+//space
+document.getElementById('key53').addEventListener('click', function() {
+    var textbox = document.getElementById('textbox');
+    textbox.value += ' ';
+});
+
+//r alt
+document.getElementById('key54').addEventListener('click', function() {
+    var textbox = document.getElementById('textbox');
+    textbox.value += '';
+});
+
+//r ctrl
+document.getElementById('key55').addEventListener('click', function() {
+    var textbox = document.getElementById('textbox');
+    textbox.value += '';
+});
