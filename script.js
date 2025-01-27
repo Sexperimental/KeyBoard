458a484c7b5d9b4214491a97a483ad29b4f313a8
experimental <65273445+sexperimental@users.noreply.github.com>
2025-01-27 14:44:31 +0900

    Update script.js

--- script.js
+++ script.js
@@ -301,10 +301,10 @@ if (isMobile()) {
 
     keyboard.style.top = '89%';
 
-    textbox.style.top = '-200%';
+    textbox.style.top = '-230%';
     textbox.style.width = '730px';
     textbox.style.height = '60px';
     textbox.style.borderRadius = "29px"
 
-    text.style.top = '-222%';
+    text.style.top = '-252%';
 }
